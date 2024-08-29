ab4f50263d61fc5d6378c8c225e2f773a3e621bb
Patata <juanjosebs.21@gmail.com>
2024-08-29 11:12:51 -0400

    Agregado el Slider de Swiper

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,37 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		linearBorderGradients: {
+		  directions: { // defaults to these values
+			't': 'to top',
+			'tr': 'to top right',
+			'r': 'to right',
+			'br': 'to bottom right',
+			'b': 'to bottom',
+			'bl': 'to bottom left',
+			'l': 'to left',
+			'tl': 'to top left',
+		  },
+		  colors: { // defaults to {}
+			'red': '#f00',
+			'red-blue': ['#f00', '#00f'],
+			'blue-green': ['#0000ff', '#00FF00'],
+			'red-green-blue': ['#f00', '#0f0', '#00f'],
+			'black-white-with-stops': ['#000', '#000 45%', '#fff 55%', '#fff'],
+		  },
+		  background: {
+			'gray-50': '#F9FAFB',
+			'gray-900':'#111827',
+		  },
+		  borders: { // defaults to these values (optional)
+			'1': '1px',
+			'2': '2px',
+			'4': '4px',
+		  },
+		},
+	
+	  },
+	  plugins: [
+		require('tailwindcss-border-gradient-radius'),
+	  ],
 }
